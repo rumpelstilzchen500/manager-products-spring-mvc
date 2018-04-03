10e8305c208576388a979690f483b2f79fe22a66
rumpel <artem.sinkovskiy@gmail.net>
2018-04-03 19:28:30 +0300

    Product service (select) web

--- src/main/java/com/rumpel/common/controller/ProductsController.java
+++ src/main/java/com/rumpel/common/controller/ProductsController.java
@@ -1,16 +1,20 @@
 package com.rumpel.common.controller;
 
+import com.rumpel.common.service.ProductServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import java.sql.SQLException;
 
 @Controller
 @RequestMapping("/products")
 public class ProductsController {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String printProducts(Model model){
+    public String printProducts(Model model) throws SQLException {
+        ProductServiceImpl productService = new ProductServiceImpl();
+        model.addAttribute("SelectAll", productService.getAll());
 
         return "products";
     }
--- src/main/webapp/WEB-INF/pages/products.jsp
+++ src/main/webapp/WEB-INF/pages/products.jsp
@@ -4,6 +4,6 @@
     <title>Title</title>
 </head>
 <body>
-
+    <h1>Select : ${SelectAll}</h1>
 </body>
 </html>
